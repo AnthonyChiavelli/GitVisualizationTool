696b8f21b72bcc25f8c0072fbe7084cb96210c5a
Anthony Reid <AnthonyReid99@gmail.com>
2014-02-24 23:15:35 -0500

    Continued basic class for commit node

--- gcommitnode.h
+++ gcommitnode.h
@@ -2,12 +2,27 @@
 #define GCOMMITNODE_H
 
 #include <QGraphicsItem>
+#include <QPainter>
 
-class GCommitNode : public QGraphicsItem
-{
+/*
+ * Graphical node representing a commit
+ */
+class GCommitNode : public QObject, public QGraphicsItem {
     Q_OBJECT
+
 public:
-    explicit GCommitNode(QObject *parent = 0);
+
+    GCommitNode(QGraphicsItem *parent);
+    // QGraphicsItems must override these two methods
+    // Returns estimate of size
+    QRectF boundingRect() const;
+    // Performs actual object rendering
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget);
+
+private:
+
+    // Size of node
+    int size;
 
 signals:
 
