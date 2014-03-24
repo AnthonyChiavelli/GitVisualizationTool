65b3dc4fc897959d9560d08380fca80df7192391
Anthony <AnthonyReid99@gmail.com>
2014-03-24 18:01:37 -0400

    fixed up errors

--- mainwindow.h
+++ mainwindow.h
@@ -1,7 +1,7 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 #include "ui_mainwindow.h"
-#include "greposcene.h"
+#include "ggraphicsscene.h"
 
 #include <QMainWindow>
 
@@ -18,7 +18,7 @@ public:
 
 private:
     Ui::MainWindow *ui;
-    GRepoScene *repoScene;
+    GGraphicsScene *scene;
 
 };
 
