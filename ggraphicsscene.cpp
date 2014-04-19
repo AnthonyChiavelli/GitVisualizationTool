e6a9b1b8275722e3f487a5cc678d61d50e6035d2
Anthony <AnthonyReid99@gmail.com>
2014-04-19 18:13:23 -0400

    Fixed up bugs caused by merge, program launches now

--- ggraphicsscene.cpp
+++ ggraphicsscene.cpp
@@ -24,13 +24,13 @@ GGraphicsScene::GGraphicsScene(QObject *parent) : QGraphicsScene(parent) {
 
 }
 
-GCommitNode *GGraphicsScene::convertCommitNodeToGCommitNode(CommitNode const & commitNode, GCommitNode const * parent, int nodeDepth) {
+GCommitNode *GGraphicsScene::convertCommitNodeToGCommitNode(CommitNode const * commitNode, GCommitNode const * parent, int nodeDepth) {
 
     // Check if this gcommit node has already been instantiated
     //TODO use getSha() once implemented
     GCommitNode *gCommitNode;
-    if(this->allGCommitNodes.find(commitNode.getAuthor().getEmail()) != allGCommitNodes.end()) {
-        gCommitNode = this->allGCommitNodes.at(commitNode.getAuthor().getEmail());
+    if(this->allGCommitNodes.find(commitNode->getAuthor().getEmail()) != allGCommitNodes.end()) {
+        gCommitNode = this->allGCommitNodes.at(commitNode->getAuthor().getEmail());
     }
     // Otherwise make a new one
     else {
@@ -43,16 +43,16 @@ GCommitNode *GGraphicsScene::convertCommitNodeToGCommitNode(CommitNode const & c
     }
 
     // Set attributes for this g node
-    gCommitNode->sha = commitNode.getAuthor().getEmail();
+    gCommitNode->sha = commitNode->getAuthor().getEmail();
     gCommitNode->depth = nodeDepth;
 
     // If there are any children, recursively call this on them
-    vector<CommitNode> children = commitNode.getChildren();
+    vector<CommitNode *> children = commitNode->getChildren();
     if (children.size() > 0) {
         // Increase the node depth for these children
         nodeDepth++;
         // Recursively call ourselves for each child and add result to our set of children
-        for (vector<CommitNode>::iterator it = children.begin(); it !=children.end(); ++it ) {
+        for (vector<CommitNode *>::iterator it = children.begin(); it !=children.end(); ++it ) {
             gCommitNode->childrenGNodes.push_back(convertCommitNodeToGCommitNode(*it, gCommitNode, nodeDepth));
         }
     }
